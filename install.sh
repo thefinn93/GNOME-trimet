2688f899927c91e102c57487946dea75dc03acc8
Finn <thefinn93@gmail.com>
2011-08-24 00:47:49 -0700

    Made the context menu work, added the beginnings of a preferences window

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cp trimet.server /usr/lib/bonobo/servers/trimet.server
-chmod +x trimet.py
 cp trimet.py /usr/bin/trimet.py
+chmod +x /usr/bin/trimet.py
 cp trimet-48.png /usr/share/icons/gnome/48x48/apps/trimet.png
 echo "Thank you for riding Trimet"
--- trimet.py
+++ trimet.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-stopid="7500"
-apikey=""
 import pygtk
 import sys
 pygtk.require('2.0')
@@ -28,13 +26,45 @@ class TransitTracker(gnomeapplet.Applet):
         self.applet.show_all()
         return 1
 
+
+    def showMenu(self,widget, event, applet):
+        if event.type == gtk.gdk.BUTTON_PRESS and event.button == 3:
+            widget.emit_stop_by_name("button_press_event")
+            self.create_menu(applet)
+        else:
+            self.button.set_label("Button press: " + str(event.button))
+            self.applet.show_all()
+
+    def create_menu(self,applet):
+        propxml="""
+			<popup name="button3">
+			<menuitem name="Item 3" verb="Preferences" label="_Preferences"/>
+			</popup>"""
+        verbs = [("Preferences", self.showPrefs)]
+        applet.setup_menu(propxml, verbs, None)
+
+    def showPrefs(self,*arguments, **keywords):
+        self.prefWindow = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        self.prefWindow.set_title("Trimet Countdown Preferences")
+        self.prefWindow.connect("destroy", gtk.main_quit)
+        self.prefWindow.set_border_width(10)
+        self.prefButton = gtk.Button("Close")
+        self.prefButton.connect_object("clicked", gtk.Widget.destroy, self.prefWindow)
+        self.prefWindow.add(self.prefButton)
+        self.prefButton.show()
+        self.prefWindow.show()
+
+
+
     def __init__(self,applet,iid):
-        self.updateinterval = 10
-        self.i = 10
+        self.stopid="7500"
+        self.apikey="7890FE8AA7CC3A7538F10BDFE"
+        self.updateinterval = 30						# Interval at which to hit the Trimet API, in seconds
+        self.i = self.updateinterval						# Number of seconds that have passed since last hit
         self.applet = applet
         self.button = gtk.Button()
         self.button.set_relief(gtk.RELIEF_NONE)
-#       self.button.connect("button_press_event", showMenu, applet)
+        self.button.connect("button_press_event", self.showMenu, applet)
         self.applet.add(self.button)
         self.applet.show_all()
         self.updateCountdown(1)
@@ -43,23 +73,6 @@ class TransitTracker(gnomeapplet.Applet):
 
 def factory(applet, iid):
     TransitTracker(applet,iid)
-
-def showMenu(widget, event, applet):
-	if event.type == gtk.gdk.BUTTON_PRESS and event.button == 3:
-		widget.emit_stop_by_name("button_press_event")
-		create_menu(applet)
-
-def create_menu(applet):
-	propxml="""
-			<popup name="button3">
-			<menuitem name="Item 3" verb="About" label="_About" pixtype="stock" pixname="gtk-about"/>
-			<menuitem name="Item 3" verb="Preferences" label="_Preferences" />
-			</popup>"""
-	verbs = [("About", showAboutDialog)]
-	applet.setup_menu(propxml, verbs, None)
-
-def showAboutDialog(*arguments, **keywords):
-	pass
 
 if len(sys.argv) == 2:
 	if sys.argv[1] == "-w":
@@ -75,4 +88,4 @@ if len(sys.argv) == 2:
 
 if __name__ == '__main__':
 	print "Starting factory"
-	gnomeapplet.bonobo_factory("OAFIID:Trimet_Factory", gnomeapplet.Applet.__gtype__, "Monitor Trimet arrvials", "1.0", factory)
+	gnomeapplet.bonobo_factory("OAFIID:GNOME_TrimetArrivals_Factory", gnomeapplet.Applet.__gtype__, "Monitor Trimet arrvials", "1.0", factory)
