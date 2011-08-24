474a5f1cedf00f300ee0a4aeba4c0e2079bd1097
Finn <thefinn93@gmail.com>
2011-08-24 01:34:08 -0700

    Added a preferences dialog and made it check if there was an ETA available before attempting to display it (it would crash before if an ETA wasn't available)

--- trimet.py
+++ trimet.py
@@ -16,13 +16,19 @@ class TransitTracker(gnomeapplet.Applet):
 
     def updateCountdown(self,event):
         if self.i >= self.updateinterval:
-            xml = parseString(urllib2.urlopen("http://developer.trimet.org/ws/V1/arrivals?locIDs=" + stopid + "&appID=" + apikey).read())
-            self.arrivaltime = int(xml.getElementsByTagName("arrival")[0].getAttribute("estimated"))/1000
+            self.arrivaltime = False
+            xml = parseString(urllib2.urlopen("http://developer.trimet.org/ws/V1/arrivals?locIDs=" + self.stopid + "&appID=" + self.apikey).read())
+            if len(xml.getElementsByTagName("arrival")) > 0:
+                if xml.getElementsByTagName("arrival")[0].getAttribute("estimated") != "":
+                    self.arrivaltime = int(xml.getElementsByTagName("arrival")[0].getAttribute("estimated"))/1000
             self.i = 0
         else:
             self.i = self.i+1
-        eta = time.gmtime(self.arrivaltime-time.time())
-        self.button.set_label(str(time.strftime("%H:%M:%S",eta)))
+        if self.arrivaltime:
+            eta = time.gmtime(self.arrivaltime-time.time())
+            self.button.set_label(str(time.strftime("%H:%M:%S",eta)))
+        else:
+            self.button.set_label("No Estimate Available")
         self.applet.show_all()
         return 1
 
@@ -44,16 +50,62 @@ class TransitTracker(gnomeapplet.Applet):
         applet.setup_menu(propxml, verbs, None)
 
     def showPrefs(self,*arguments, **keywords):
-        self.prefWindow = gtk.Window(gtk.WINDOW_TOPLEVEL)
-        self.prefWindow.set_title("Trimet Countdown Preferences")
-        self.prefWindow.connect("destroy", gtk.main_quit)
-        self.prefWindow.set_border_width(10)
-        self.prefButton = gtk.Button("Close")
-        self.prefButton.connect_object("clicked", gtk.Widget.destroy, self.prefWindow)
-        self.prefWindow.add(self.prefButton)
-        self.prefButton.show()
-        self.prefWindow.show()
+        self.prefsWindow = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        self.prefsWindow.set_title("Trimet Countdown Preferences")
+        self.prefsWindow.connect("destroy", gtk.main_quit)
+        self.prefsWindow.set_border_width(10)
 
+        mainBox = gtk.VBox()
+
+        apibox = gtk.HBox()
+        apilabel = gtk.Label("Trimet API key:")
+        apibox.pack_start(apilabel,False,False,0)
+        apilabel.show()
+        self.prefsAPIkey = gtk.Entry(max=25)
+        self.prefsAPIkey.set_text(self.apikey)
+        self.prefsAPIkey.connect("focus_out_event",self.savePrefs)
+        apibox.pack_start(self.prefsAPIkey,True,False,0)
+        self.prefsAPIkey.show()
+        mainBox.pack_start(apibox,False,False,0)
+        apibox.show()
+
+        stopIDbox = gtk.HBox()
+        stopIDlabel = gtk.Label("Trimet Stop ID:")
+        stopIDbox.pack_start(stopIDlabel,False,False,0)
+        stopIDlabel.show()
+        self.prefsStopID = gtk.Entry(max=0)
+        self.prefsStopID.set_text(self.stopid)
+        self.prefsStopID.connect("focus_out_event",self.savePrefs)
+        stopIDbox.pack_start(self.prefsStopID,True,False,0)
+        self.prefsStopID.show()
+        mainBox.pack_start(stopIDbox,False,False,0)
+        stopIDbox.show()
+
+        updateintervalbox = gtk.HBox()
+        updateintervallabel = gtk.Label("Update Interval:")
+        updateintervalbox.pack_start(updateintervallabel,False,False,0)
+        updateintervallabel.show()
+        self.prefsupdateinterval = gtk.Entry(max=0)
+        self.prefsupdateinterval.set_text(str(self.updateinterval))
+        self.prefsupdateinterval.connect("focus_out_event",self.savePrefs)
+        updateintervalbox.pack_start(self.prefsupdateinterval,True,False,0)
+        self.prefsupdateinterval.show()
+        mainBox.pack_start(updateintervalbox,False,False,0)
+        updateintervalbox.show()
+
+        self.prefsClose = gtk.Button("Close")
+        self.prefsClose.connect_object("clicked", gtk.Widget.destroy, self.prefsWindow)
+        mainBox.pack_start(self.prefsClose,False,False,0)
+        self.prefsClose.show()
+
+        mainBox.show()
+        self.prefsWindow.add(mainBox)
+        self.prefsWindow.show()
+
+    def savePrefs(self,widget,data):
+        self.apikey = self.prefsAPIkey.get_text()
+        self.stopid = self.prefsStopID.get_text()
+        self.updateinterval = int(self.prefsupdateinterval.get_text())
 
 
     def __init__(self,applet,iid):
